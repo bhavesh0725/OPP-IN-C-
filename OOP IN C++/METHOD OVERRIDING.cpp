d3457660f99242346e8745714b389eb7869b3201
Bhavesh S Amborkar <BHAVESHVIRAT06@GMAIL.COM>
2024-02-03 15:44:15 +0530

    Create METHOD OVERRIDING.cpp

---
+++ OOP IN C++/METHOD OVERRIDING.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class hero {
+public:
+  int age;
+  string name;
+  int a;
+
+public:
+  void action() { cout << "Hero's action"<<endl; }
+};
+
+
+
+class villan : public  hero {
+public:
+  void action() { cout << "Villan's action"<<endl; }
+};
+
+
+
+int main() {
+
+  hero ram;
+  villan ravan;
+  ram.action();
+  ravan.action();
+    
+}
